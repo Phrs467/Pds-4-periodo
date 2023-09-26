f90e32616ae6b085c4988366555d12f5463ad10b
Pedro Henrique <phrs467@gmail.com>
2023-09-26 19:47:12 -0300

    pds 4 periodo/2

---
+++ src/app/darktheme/darktheme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-darktheme',
+  templateUrl: './darktheme.component.html',
+  styleUrls: ['./darktheme.component.css']
+})
+export class DarkthemeComponent {
+
+}
